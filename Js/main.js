58cb7d68a6defc077faaf7e773c62be2a0b2475b
mohamed-wael74 <mohamedwaelol74@gmail.com>
2022-02-07 01:48:00 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $("body").niceScroll();
+})
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
+
+
+jQuery("#responsive_headline").fitText();
+
+
+new WOW().init();
